ca757ea1733085f8ee57e59bc44a74106a455398
leon.feder <leonfeder18@gmail.com>
2023-08-15 13:28:56 +0200

    we

--- src/GUI/Preisrechner.java
+++ src/GUI/Preisrechner.java
@@ -7,28 +7,56 @@ import java.awt.event.ActionListener;
 public class Preisrechner {
     private JPanel mainpannel;
     private JLabel Preisrechner;
-    private JLabel Wort1;
     private JButton bestätigenButton;
     private JTextField Textanzahl;
-    private JTextField Wort1Text;
+    private JTextField Eingabefeld;
+    private JButton mal;
     private JTextField WortEndText;
-    private JLabel WortEnd;
     private JButton minus;
     private JButton plus;
     private JButton geteilt;
 
     public Preisrechner() {
+        double ersteZahl;
+        double zweiteZahl= 0;
+        int zeichen= 0;
         bestätigenButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                String wort = Wort1Text.getText();
-                String wortvorher = WortEndText.getText();
-                String gesamtpreis= wortvorher+wort;
-                WortEndText.setText(String.valueOf(gesamtpreis));
+
+
+                double gesamtpreis= ersteZahl+zweiteZahl;
+                Eingabefeld.setText(String.valueOf(gesamtpreis));
             }
         });
 
 
+        plus.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(null == ersteZahl){
+                    ersteZahl=Double.valueOf(Eingabefeld.getText());
+                };
+            }
+        });
+        minus.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        mal.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        geteilt.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
     }
 
     public static void main(String[] args) {
