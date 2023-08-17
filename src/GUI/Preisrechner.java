b4b6d59b22104cf715ee7884b1a6a8c7888a9f7e
leon.feder <leonfeder18@gmail.com>
2023-08-17 09:37:01 +0200

    w

--- src/GUI/Preisrechner.java
+++ src/GUI/Preisrechner.java
@@ -17,43 +17,102 @@ public class Preisrechner {
     private JButton geteilt;
 
     public Preisrechner() {
-        double ersteZahl;
-        double zweiteZahl= 0;
-        int zeichen= 0;
+        final double[] ersteZahl = {0};
+        final boolean[] ersteZahlvoll = {false};
+         ersteZahlvoll[0]=false;
+
+        final int[] zeichen = {0};
         bestätigenButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                if(Eingabefeld.getText()!=null) {
+                    double zweiteZahl = Double.valueOf(Eingabefeld.getText());
+                }
+                double zweiteZahl = Double.valueOf(Eingabefeld.getText());
+                if(ersteZahlvoll[0]=true){
+                //    double zweiteZahl =Double.valueOf(Eingabefeld.getText());
+                if(zeichen[0]==1) {
+                    double gesamtpreis = ersteZahl[0] + zweiteZahl;
+                    Eingabefeld.setText(String.valueOf(gesamtpreis));
+                    ersteZahl[0] = 0;
+
+                }
+                    if(zeichen[0]==2) {
+                        double gesamtpreis = ersteZahl[0] - zweiteZahl;
+                        Eingabefeld.setText(String.valueOf(gesamtpreis));
+                        ersteZahl[0] = 0;
+
+                    }
+                    if(zeichen[0]==3) {
+                        double gesamtpreis = ersteZahl[0] * zweiteZahl;
+                        Eingabefeld.setText(String.valueOf(gesamtpreis));
+
+                        ersteZahl[0] = 0;
 
 
-                double gesamtpreis= ersteZahl+zweiteZahl;
-                Eingabefeld.setText(String.valueOf(gesamtpreis));
+                    }
+                    if(zeichen[0]==4) {
+                        if(zweiteZahl!=0) {
+                            double gesamtpreis = ersteZahl[0] / zweiteZahl;
+                            Eingabefeld.setText(String.valueOf(gesamtpreis));
+                            ersteZahl[0] = 0;
+                        }else{Eingabefeld.setText("ERROR DIVISION DURCH 0 ");
+
+                    }
+              } else if (ersteZahlvoll[0]=false) {
+                    Eingabefeld.setText("Bitte zwei Zahlen nund Zeichen eingeben");
+                    try {
+                        Thread.sleep(3000);
+                    } catch (InterruptedException ex) {
+                        throw new RuntimeException(ex);
+                    }
+                    Eingabefeld.setText("");
+
+
+
+
+
+                }
             }
-        });
+        }});
 
         plus.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                if(null== ersteZahl){
-                    ersteZahl=Double.valueOf(Eingabefeld.getText());
-                };
+
+             double ersteZahl1 =Double.valueOf(Eingabefeld.getText());
+               Eingabefeld.setText("");
+               ersteZahl[0] =ersteZahl1;
+               zeichen[0] = 1;
+               ersteZahlvoll[0] = true;
+
             }
         });
         minus.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
+                double ersteZahl1 =Double.valueOf(Eingabefeld.getText());
+                Eingabefeld.setText("");
+                ersteZahl[0] =ersteZahl1;
+                zeichen[0] = 2;
             }
         });
         mal.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
+                double ersteZahl1 =Double.valueOf(Eingabefeld.getText());
+                Eingabefeld.setText("");
+                ersteZahl[0] =ersteZahl1;
+                zeichen[0] = 3;
             }
         });
         geteilt.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
+                double ersteZahl1 =Double.valueOf(Eingabefeld.getText());
+                Eingabefeld.setText("");
+                ersteZahl[0] =ersteZahl1;
+                zeichen[0] = 4;
             }
         });
     }
